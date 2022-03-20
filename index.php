bfae5f20210a938ff7fb0498d01c764ea4d57902
KryLex0 <adam.mkh@hotmail.fr>
2022-03-20 21:37:30 +0100

    Modification du code
    Implémentation de la partie pagination en plus de l'upload

--- index.php
+++ index.php
@@ -1,56 +1,180 @@
-<?php
-
-$pathParent = dirname(__FILE__);
-include $pathParent . "/credentials/credentials.php";
-
-header('Content-Type: text/html; charset=utf-8');
-
-?>
 <!DOCTYPE html>
 <link rel="stylesheet" href="style/upload.css">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="script/script.js"></script>
+
 
 <h1 style="text-align:center;">Upload</h1>
 
 <?php
 
+$pathParent = dirname(__FILE__);
+require_once "credentials/credentials.php";
+
+//appel d'un fichier contenant les fonctions de pagination
+require_once "pagination.php";
+
+//connexion a la BDD
+$mysqlClient = new PDO($dbname, $login, $password);
+
+//si aucun paramètre n'est passé dans l'url, redirige automatiquement vers la page numéro 1
+if(!$_GET["numPage"]){
+    header("LOCATION: ?numPage=1");
+    exit();
+}
+
 //taille max de 8 Mo
 $tailleMaxFichier = 8 * 1024 * 1024;
 //chemin des fichiers upload
-$cheminDest = $pathParent . "\uploadFiles";
+$cheminDest = "uploadFiles";
+$pathImage = $pathParent . "\\" . $cheminDest;
+
 //type de fichier prit en charge
-$extensionAccepte = array("png", "jpg", "jpeg", "pdf");
+$extensionAccepte = array("png", "jpg", "jpeg");
+//nom de la table dans laquelle récupérer les données
+$tableName = "uploadfilesdata";
+
+//nombre d'element à afficher par page
+$nbElemPage = 6;
+
+//permet d'obtenir le nombre de page qui contiendront 6 elements de la BDD par page (58/6 arrondi au supérieur donne 10 pages de 6 élements max) 
+if(ceil(getDatabaseCount($mysqlClient, $tableName)/$nbElemPage) == 0){
+    $nbPageData = 1;
+}else{
+    $nbPageData = ceil(getDatabaseCount($mysqlClient, $tableName)/$nbElemPage);
+}
+
+//offset qui permet d'obtenir les lignes de données à partir du numéro de page (page 2 => de l'élement 7 à ...)
+$numDepartElem = $nbElemPage * $_GET['numPage'] - $nbElemPage;
+//si l'utilisateur change le numéro de page vers un nombre trop grand (sans données donc), redirige vers la dernière page contenant des données
+verifLastPage($mysqlClient, $tableName, $nbElemPage, $numDepartElem, $nbPageData);
+
+
+//affiche un message en tête de page suivant la réussite ou non de l'upload grâce à un paramètre présent dans l'url
+if(isset($_GET["uploadSuccess"])){
+    if($_GET["uploadSuccess"] == "true"){
+        echo "<p style='text-align:center;background-color:green'>Upload du fichier réussit.</p>";
+    }elseif($_GET["uploadSuccess"] == "false"){
+        echo "<p style='text-align:center;background-color:yellow'>L'image est déjà présente dans la BDD.</p>";
+    }elseif($_GET["uploadSuccess"] == "extension"){
+        echo "<p style='text-align:center;background-color:orange'>Le type de fichier n'est pas prit en charge.</p>";
+    }elseif($_GET["uploadSuccess"] == "error"){
+        echo "<p style='text-align:center;background-color:red'>Une erreur est survenue lors de l'upload. Veuillez réessayer.</p>";
+    }
+    
+
+}
+
 
 ?>
 
+<!--Grille contenant les images-->
     <div class="divClass">
-        <div class="divContainer">
-            <form action="" method="post" enctype="multipart/form-data">
-                <input type="hidden" name="MAX_FILE_SIZE" value="<?php echo $tailleMaxFichier;?>">
-                <label>Votre fichier: </label>
-                <input type="file" name="nomFichier"><br>
-                <label>Nouveau nom du fichier: </label><input type="text" name="nouveauNomFichier" placeholder="Nom d'origine si vide"><br><br>
-                <input type="submit" value="Envoyer">
-            </form>
-        </div>
-        <p>Type de fichier prit en charge: <?php foreach($extensionAccepte as $val){echo $val . ", ";} ?></p>
+        <?php
+        $i=0;
+        
+        //affiche les élements retournés de la requete SQL
+        $dataFromDatanase = getDataDatabase($mysqlClient, $tableName, $nbElemPage, $numDepartElem);
+        if(empty($dataFromDatanase)){
+            echo "<p><strong>Aucunes images n'est présente dans la Base de Données.</strong></p>";
+        }else{
+            foreach($dataFromDatanase as $tab => $val) {
+                $i++;
+            ?>
+                <div class="divClassData">
+                    <p><strong style="text-decoration: underline;">Nom image:</strong><?php echo(" " . $val["nom_fichier"] . "." . $val["extension_fichier"]); ?></p>
+                    <img class="image" src="<?php echo $cheminDest . "/" . $val["nom_fichier"] . "." . $val["extension_fichier"]; ?>" onClick="$(this).toggleClass('zoomed');">
+                    <p><strong style="text-decoration: underline;">Taille de l'image:</strong><?php echo(" " . $val["taille_image"] . " ko");?></p>
+                </div>
+
+            <?php 
+                //toutes les 2 itérations, revient à la ligne afin d'avoir 2 éléments par ligne
+                if($i%2==0){
+                    ?>
+                    <div style='clear:both'></div>
+                    <?php
+                }
+            }
+        }
+        ?>
     </div>
 
-    </br>
+
+  <!--Bouton de numéro de page
+1, 2, [3], 4, 5, ..., 30-->
+<div class="changePage">
+    <div class="changePageButton">
+        <!-- Affiche un bouton qui redirige vers la page 1 lorsqu'on se trouve sur une autre page que la page 1 -->
+        <?php if($_GET["numPage"] != 1){?>
+            <a href=<?php echo getPageUrlByNumber(1); ?> class="btn btn-info"><?php echo (1); ?></a>
+        <?php } ?>
+        <?php
+        //if($_GET['numPage'] > 1 && $_GET['numPage'] < $nbPageData){
+            for($i=-2; $i<3; $i++){
+                //s'il y a une différence > 2 entre la 1ère page et la page actuelle (ou entre la dernière page et la page actuelle), affiche [...]
+                if($i == -2 && $_GET["numPage"] + $i > 2 || $i == 2 && $_GET["numPage"] + $i < $nbPageData - 2 ){
+                    ?><a class="btn btn-info" readonly><?php echo("..."); ?></a><?php
+                    
+                }
+                //affiche le numéro de page actuel
+                elseif($i == 0){
+                    ?><strong><a class="btn btn-info" style="background-color:grey"><?php echo ($_GET["numPage"] + $i); ?></a></strong><?php
+                    
+                //affiche les numéros de pages précédents et suivants autour de la page actuelle
+                }elseif($_GET["numPage"] + $i > 1 && $_GET["numPage"] + $i < $nbPageData ){
+        ?>
+                <a href=<?php echo getPageUrlByNumber($_GET["numPage"] + $i); ?> class="btn btn-info"><?php echo ($_GET["numPage"] + $i); ?></a>
+
+        <?php
+                }
+            }
+
+        //Affiche un bouton qui redirige vers la dernière page lorsqu'on se trouve sur une autre page que la dernière
+        if($_GET["numPage"] != $nbPageData){?>
+            <a href=<?php echo getPageUrlByNumber($nbPageData); ?> class="btn btn-info"><?php echo ($nbPageData); ?></a>
+        <?php } ?>
+    </div>
+</div>
+
+
+<!--Formulaire d'upload d'une image-->
+
+<div class="divClass">
+    <div class="divContainer">
+        <form action="" method="post" enctype="multipart/form-data">
+            <input type="hidden" name="MAX_FILE_SIZE" value="<?php echo($tailleMaxFichier); ?>">
+            <label>Votre fichier: </label>
+            <input type="file" name="nomFichier"><br>
+            <label>Nouveau nom du fichier: </label><input type="text" name="nouveauNomFichier" placeholder="Nom d'origine si vide"><br><br>
+            <input type="submit" value="Envoyer">
+        </form>
+    </div>
+    <p>Type de fichier prit en charge:
+        <!--Affiche les extensions prises en charge avec une virgule pour séparer (sauf le dernier élément)-->
+        <?php foreach($extensionAccepte as $val){
+            if (array_search($val, $extensionAccepte) == array_key_last($extensionAccepte)){
+                echo $val;
+            }else{
+                echo $val . ", ";
+            }
+        } ?>
+    </p>
+</div>
+
+
 
     <?php
 
-    
     //si un fichier est bien envoyé via le formulaire
     if(isset($_FILES["nomFichier"])){
         //si aucune erreur n'est présente lors de l'upload du fichier
         if ($_FILES["nomFichier"]["error"] == 0) {
             //permet d'obtenir l'extension
             $array = explode('.', $_FILES['nomFichier']['name']);
-            $extension = end($array);
-            echo $extension;
+            $extension = strtolower(end($array));
+            $tailleImage = $_FILES["nomFichier"]["size"];
+            
             if(in_array($extension, $extensionAccepte)){
                 //si l'input du nouveau nom est vide, garde le nom d'origine du fichier
                 if($_POST["nouveauNomFichier"] == null){
@@ -60,33 +184,47 @@ $extensionAccepte = array("png", "jpg", "jpeg", "pdf");
                 //sinon, attribue le nouveau nom entré au fichier
                 }else{               
                     $nomFichier = $_POST["nouveauNomFichier"];
-                    
                 }
-                //upload le fichier dans un dossier "/uploadFiles"
-                move_uploaded_file($_FILES["nomFichier"]["tmp_name"], $cheminDest . "\\" . $nomFichier . "." . $extension);
                 try {
-                    //connexion a la BDD
-                    $mysqlClient = new PDO($dbname, $login, $password);
-                    //ajoute des antislash devant tout les caractères spéciaux de l'url
-                    $cheminDest = addcslashes($cheminDest, "\\");
-                    //insertion des données dans la BDD
-                    $sqlQuery = "INSERT INTO uploadfilesdata(nom_fichier, extension_fichier, chemin_fichier) VALUES ('$nomFichier', '$extension', '".$cheminDest."')";
-                    $result = $mysqlClient->prepare($sqlQuery);
-                    $result->execute();
+                    if(!dataIsInDB($mysqlClient, $tableName, "nom_fichier", $nomFichier)){
+                        //insertion des données dans la BDD
+                        $sqlQuery = "INSERT INTO uploadfilesdata(nom_fichier, extension_fichier, chemin_fichier, taille_image) VALUES ('$nomFichier', '$extension', '".$cheminDest."', '$tailleImage')";
+                        $result = $mysqlClient->prepare($sqlQuery);
+                        $result->execute();
+                        //upload le fichier dans un dossier "/uploadFiles"
+                        move_uploaded_file($_FILES["nomFichier"]["tmp_name"], $pathImage . "\\" . $nomFichier . "." . $extension);
+                        
+                        //redirection vers la même page avec des paramètre pour savoir si l'upload est réussit ou non
+                        $URL=strtok($_SERVER["REQUEST_URI"], '?') . "?numPage=1&uploadSuccess=true";
+                        echo "<script type='text/javascript'>document.location.href='{$URL}';</script>";
+                        echo '<META HTTP-EQUIV="refresh" content="0;URL=' . $URL . '">';
+                    }else{
+                        $URL=strtok($_SERVER["REQUEST_URI"], '?') . "?numPage=1&uploadSuccess=false";
+                        echo "<script type='text/javascript'>document.location.href='{$URL}';</script>";
+                        echo '<META HTTP-EQUIV="refresh" content="0;URL=' . $URL . '">';
+                        
+                    }
                 } catch(PDOException $e) {
                     die('Erreur de connexion à la BDD. Erreur n°' . $e->getCode() . ':' . $e->getMessage());
                 }
                 //affiche un message d'upload réussi
                 ?>
-                <p style="text-align:center;">Upload du fichier <strong>'<?php echo $nomFichier; ?>'</strong> réussit.</p><?php
-            }else{?>
-                <p style="text-align:center;"><strong>L'extension du fichier n'est pas prit en charge.</strong></p><?php
+                <p style="text-align:center;">Upload du fichier <strong>'<?php echo $nomFichier . "." . $extension; ?>'</strong> réussit.</p>
+
+                <?php
+                //affiche un problème concernant le type de document ajouté qui n'est pas prit en charge
+            }else{
+                $URL=strtok($_SERVER["REQUEST_URI"], '?') . "?numPage=1&uploadSuccess=extension";
+                echo "<script type='text/javascript'>document.location.href='{$URL}';</script>";
+                echo '<META HTTP-EQUIV="refresh" content="0;URL=' . $URL . '">';
             }
-        //sinon, affiche un message d'erreur
-        }else{?>
-            <p style="text-align:center;">Une erreur est survenue. Veuillez réessayer.</p><?php
+        }else{
+                $URL=strtok($_SERVER["REQUEST_URI"], '?') . "?numPage=1&uploadSuccess=error";
+                echo "<script type='text/javascript'>document.location.href='{$URL}';</script>";
+                echo '<META HTTP-EQUIV="refresh" content="0;URL=' . $URL . '">';
         }
 
     }
-    
+
+
     ?>
---
+++ pagination.php
@@ -0,0 +1,92 @@
+<?php
+
+
+
+
+
+//si les données retournés de la requete sont vides, modifie une variable de l'url et affiche la dernière page possédant des résultats 
+/*
+if(!getImagesDatabase($mysqlClient, $nbElemPage, $numDepartElem)){
+    if($_GET["numPage"] > $nbPageData){
+        $query = $_GET;
+        $query['numPage'] = $nbPageData;
+        $query_result = http_build_query($query);
+
+        header("LOCATION: ?$query_result");
+    }
+}
+*/
+
+//fonction qui retourne le nombre de lignes dans la BDD
+function getDatabaseCount($mysqlClient, $table){
+    $sqlQueryCount = "SELECT COUNT(*) FROM $table";
+
+    $result = $mysqlClient->prepare($sqlQueryCount);
+    $result->execute();
+    $databaseCount = $result->fetchAll();
+    return $databaseCount[0][0];
+}
+
+function getDataDatabase($mysqlClient, $table, $nbElemPage, $numDepartElem){
+    //requete pour obtenir les X lignes de la BDD en fonction de l'offset (la page ou l'on se trouve)
+    //avec une limite fixé du nombre de résultat à retourner
+    $sqlQueryData = "SELECT * FROM $table ORDER BY id DESC LIMIT $nbElemPage OFFSET $numDepartElem";
+
+    $result = $mysqlClient->prepare($sqlQueryData);
+    $result->execute();
+    $dataDatabase = $result->fetchAll();
+    return $dataDatabase;
+}
+
+//fonction qui permet d'obtenir l'url de la page précédente
+function getPreviousPageUrl(){
+    $previousPageNumber = $_GET["numPage"] - 1;
+    $baseUrl = strtok($_SERVER['REQUEST_URI'], '?');
+    $previousPageUrl = $baseUrl . "?numPage=" . $previousPageNumber;
+    return $previousPageUrl;
+}
+
+//fonction qui permet d'obtenir l'url de la page suivante
+function getNextPageUrl(){
+    $nextPageNumber = $_GET["numPage"] + 1;
+    $baseUrl = strtok($_SERVER['REQUEST_URI'], '?');
+    $nextPageUrl = $baseUrl . "?numPage=" . $nextPageNumber;
+    return $nextPageUrl;
+}
+
+//fonction qui permet d'obtenir une url en fonction du numéro de page passé en paramètre
+function getPageUrlByNumber($numNewPage){
+    $baseUrl = strtok($_SERVER['REQUEST_URI'], '?');
+    $nextPageUrl = $baseUrl . "?numPage=" . $numNewPage;
+    return $nextPageUrl;
+}
+
+//si les données retournés de la requete sont vides, modifie une variable de l'url et affiche la dernière page possédant des résultats 
+function verifLastPage($mysqlClient, $table, $nbElemPage, $numDepartElem, $nbPageData){
+    if(!getDataDatabase($mysqlClient, $table, $nbElemPage, $numDepartElem)){
+        if($_GET["numPage"] > $nbPageData){
+            $query = $_GET;
+            $query['numPage'] = $nbPageData;
+            $query_result = http_build_query($query);
+
+            header("LOCATION: ?$query_result");
+            exit();
+        }
+    }
+}
+
+//vérifie si une image est déjà présente dans la BDD
+function dataIsInDB($mysqlClient, $table, $nomColonne, $donneeLigne){
+    $sqlQueryData = "SELECT * FROM $table WHERE $nomColonne='$donneeLigne'";
+    
+    $result = $mysqlClient->prepare($sqlQueryData);
+    $result->execute();
+    $dataDatabase = $result->fetchAll();
+    $isInDB = false;
+    if(!empty($dataDatabase)){
+        $isInDB = true;
+        echo "boucle";
+    }
+    return $isInDB;
+}
+?>
